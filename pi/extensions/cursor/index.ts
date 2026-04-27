6dfbc3eced4253d2ea447ccd42dd24bd9b62f57f
Rui Caridade <ruidanielcaridade@gmail.com>
2026-04-27 13:12:11 +0100

    Scaffold pi-cursor extension package

---
+++ pi/extensions/cursor/index.ts
@@ -0,0 +1,5 @@
+import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
+
+export default async function cursorExtension(_pi: ExtensionAPI): Promise<void> {
+  // Filled in across subsequent tasks.
+}
